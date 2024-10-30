c56e14ae03a52698d2dd12b34dff023a377f41aa
DasTapan <tapands.das@gmail.com>
2024-10-30 23:38:25 +0530

    Refactor to include new line or comma

--- lib/script.rb
+++ lib/script.rb
@@ -2,13 +2,8 @@ class StringCalculator
     def add(input)
         if input.empty?
             0
-        elsif input.split(",").length == 1
-            input.to_i
-        elsif input.split(",").length == 2
-            num_one, num_two = input.split(",")
-            num_one.to_i + num_two.to_i
         else
-            nums = input.split(",")
+            nums = input.split(/[\n,]/)
             nums.reduce(0) {|sum,n| sum + n.to_i}
         end
     end
--- spec/script_spec.rb
+++ spec/script_spec.rb
@@ -3,6 +3,7 @@ require './lib/script'
 describe StringCalculator do
     describe "#add" do
         string_calculator = StringCalculator.new
+
         it "returns 0 in case of empty string" do
             expect(string_calculator.add("")).to eql(0)
         end
@@ -15,8 +16,16 @@ describe StringCalculator do
             expect(string_calculator.add("100,1")).to eql(101)
         end
 
+        it "returns the sum of two numbers separated by new line" do 
+            expect(string_calculator.add("12\n2")).to eql(14)
+        end
+
         it "returns the sum of unknown amount of numbers" do
             expect(string_calculator.add("10,2,0,4")).to eql(16)
+        end
+
+        it "returns the sum of numbers separated by comma or new line" do
+            expect(string_calculator.add("1\n2,3,4")).to eql(10)
         end
     end
 end
